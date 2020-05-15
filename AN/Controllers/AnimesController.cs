0b4582bc4dad2536a5c0f14e300f8eaab30991ee
Barbara Ezomo <barbie.ezomo@gmail.com>
2020-05-15 13:29:58 +0100

    added a get anime action

---
+++ AN/Controllers/AnimesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using AN.Core;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AN.Controllers
+{
+    [Route("api/v1/anime")]
+    [ApiController]
+    [Authorize]
+    public class AnimesController : Controller
+    {
+        public IUnitOfWork _unitOfWork { get; set; }
+        private readonly ILogger<AnimesController> _logger;
+
+        public AnimesController(ILogger<AnimesController> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+
+        [Produces("application/json")]
+        [HttpGet]
+        public IActionResult GetAnimes()
+        {
+            return Ok(_unitOfWork.Animes.GetAll());
+        }
+
+    }
+}
